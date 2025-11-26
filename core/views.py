6c78664e69c8459888606c04e7a7ced337b430aa
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-26 15:27:10 +0000

    Add very simple placeholder home template to test Django connection

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """
+    Temporary placeholder home view.
+    This will eventually become the main dashboard.
+    """
+    return render(request, "core/home.html")
