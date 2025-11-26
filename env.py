973361a7b8db25810aa7da91d6159840a5f418f9
Ceri-Jane <ceri.jane.89@gmail.com>
2025-11-26 16:53:47 +0000

    Move SECRET_KEY to environment variables and secure settings.py

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "@z*%&(-_5*#ljmj#3m8!8f_@o-4(4v4@&pjy$&m*(sj0t(g)te")
--- regulate_project/settings.py
+++ regulate_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import env
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ho#*jozxnzr237w(dyk=cv5bwb=^@80q!_&5ou0-w#72*&emp9'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
