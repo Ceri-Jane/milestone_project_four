d259302c370cc329dae7d0df3099e917b02aff86
Ceri-Jane <ceri.jane.89@gmail.com>
2025-12-02 19:05:04 +0000

    Add dashboard route to core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),  # homepage / landing
     path("new-entry/", views.new_entry, name="new_entry"),  # new entry page
+    path("dashboard/", views.dashboard, name="dashboard"),  # dashboard page
 ]
