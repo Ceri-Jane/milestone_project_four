16a0d262bbe2771132eeaf266d608750137ceb5b
Ceri-Jane <ceri.jane.89@gmail.com>
2026-01-21 15:27:52 +0000

    Improve admin view for subscriptions

--- billing/admin.py
+++ billing/admin.py
@@ -20,14 +20,26 @@ class SubscriptionAdmin(admin.ModelAdmin):
         "stripe_subscription_id",
     )
     list_filter = ("status", "has_had_trial")
-    search_fields = ("user__username", "user__email", "stripe_customer_id")
+    search_fields = ("user__username", "user__email", "stripe_customer_id", "stripe_subscription_id")
+    ordering = ("-created_at",)
+
     readonly_fields = (
+        "user",
         "stripe_customer_id",
         "stripe_subscription_id",
+        "has_had_trial",
+        "status",
         "trial_end",
         "current_period_end",
         "created_at",
         "updated_at",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("user", "status", "has_had_trial")}),
+        ("Dates", {"fields": ("trial_end", "current_period_end")}),
+        ("Stripe", {"fields": ("stripe_customer_id", "stripe_subscription_id")}),
+        ("System", {"fields": ("created_at", "updated_at")}),
     )
 
 
@@ -35,13 +47,24 @@ class SubscriptionInline(admin.StackedInline):
     model = Subscription
     can_delete = False
     extra = 0
+    max_num = 1
+
     readonly_fields = (
         "stripe_customer_id",
         "stripe_subscription_id",
+        "status",
+        "has_had_trial",
         "trial_end",
         "current_period_end",
         "created_at",
         "updated_at",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("status", "has_had_trial")}),
+        ("Dates", {"fields": ("trial_end", "current_period_end")}),
+        ("Stripe", {"fields": ("stripe_customer_id", "stripe_subscription_id")}),
+        ("System", {"fields": ("created_at", "updated_at")}),
     )
 
 
@@ -49,6 +72,9 @@ class CustomUserAdmin(UserAdmin):
     inlines = [SubscriptionInline]
 
 
-# Unregister default User admin and re-register with inline
-admin.site.unregister(User)
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
 admin.site.register(User, CustomUserAdmin)
