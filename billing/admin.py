d06a515bc4689329d670e63810e811064eda1f9c
Ceri-Jane <ceri.jane.89@gmail.com>
2026-01-21 15:18:15 +0000

    Add subscription management to Django admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Subscription
+
+User = get_user_model()
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "status",
+        "has_had_trial",
+        "trial_end",
+        "current_period_end",
+        "stripe_customer_id",
+        "stripe_subscription_id",
+    )
+    list_filter = ("status", "has_had_trial")
+    search_fields = ("user__username", "user__email", "stripe_customer_id")
+    readonly_fields = (
+        "stripe_customer_id",
+        "stripe_subscription_id",
+        "trial_end",
+        "current_period_end",
+        "created_at",
+        "updated_at",
+    )
+
+
+class SubscriptionInline(admin.StackedInline):
+    model = Subscription
+    can_delete = False
+    extra = 0
+    readonly_fields = (
+        "stripe_customer_id",
+        "stripe_subscription_id",
+        "trial_end",
+        "current_period_end",
+        "created_at",
+        "updated_at",
+    )
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [SubscriptionInline]
+
+
+# Unregister default User admin and re-register with inline
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
