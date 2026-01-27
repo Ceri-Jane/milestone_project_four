a96cd673c0c311e578beaf5f9935054f42093c4d
Ceri-Jane <ceri.jane.89@gmail.com>
2026-01-27 14:23:32 +0000

    Add Regulate+ route

--- billing/urls.py
+++ billing/urls.py
@@ -4,6 +4,9 @@ from . import webhooks
 
 # Billing and subscription routes
 urlpatterns = [
+    # Regulate+ hub (trial / upgrade / manage billing)
+    path("regulate-plus/", views.regulate_plus, name="regulate_plus"),
+
     # Start free trial checkout
     path("start-trial/", views.start_trial, name="start_trial"),
 
@@ -17,6 +20,6 @@ urlpatterns = [
     # Stripe billing portal
     path("manage/", views.billing_details, name="billing_details"),
 
-    # Stripe webhook endpoint
+    # Stripe webhook endpoint (DO NOT TOUCH)
     path("webhook/", webhooks.stripe_webhook, name="stripe_webhook"),
 ]
